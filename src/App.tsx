5daf896069ddcfe339fa74974fe99e87a1e0063b
Abdurauf Ashurov <aabdurauf004@gmail.com>
2023-07-05 20:42:29 +0500

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-import Input from "./components/Input";
-import ToDoItem from "./components/ToDoItem";
-import { Button } from "react-bootstrap";
+import { Button, ButtonGroup } from "react-bootstrap";
+import { ToastContainer, toast } from "react-toastify";
 
 interface TodoItem {
   id: string;
@@ -22,35 +21,51 @@ const App: React.FC = () => {
       };
       setTodos([...todos, newTodo]);
       setInputVal("");
+      toast.success("Task added successfully");
+    } else {
+      toast.error("Please provide value");
     }
   };
 
   const deleteItem = (id: string): void => {
     const updateTodos: TodoItem[] = todos.filter(todo => todo.id !== id);
     setTodos(updateTodos);
+    toast.success("Task deleted successfully");
+  };
+
+  const editItem = (id: string): void => {
+    console.log(id);
   };
 
   return (
     <div className="background">
+      <ToastContainer position="top-left" autoClose={2000} />
       <div className="container mx-auto">
         <div className="row col-6 mx-auto mt-5">
-          <Input onChange={e => setInputVal(e.target.value)} value={inputVal} />
-          <Button color="primary" onClick={addItem}>
+          <input onChange={e => setInputVal(e.target.value)} value={inputVal} />
+          <Button className="btn-primary" onClick={addItem}>
             Add task
           </Button>
         </div>
         <ul className="list-group my-5">
           {todos.map(todo => (
-            <ToDoItem key={todo.id}>
-              <div>
-                <span>{todo.text}</span>
-              </div>
-              <div>
+            <li
+              className="list-group-item d-flex align-items-center justify-content-between"
+              key={todo.id}
+            >
+              <span>
+                <input type="checkbox" />
+                {todo.text}
+              </span>
+              <ButtonGroup>
+                <Button className="btn-success" onClick={() => editItem(todo.id)}>
+                  Edit task
+                </Button>
                 <Button className="btn-danger" onClick={() => deleteItem(todo.id)}>
                   Delete task
                 </Button>
-              </div>
-            </ToDoItem>
+              </ButtonGroup>
+            </li>
           ))}
         </ul>
       </div>
