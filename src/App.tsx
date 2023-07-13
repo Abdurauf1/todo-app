051ea1cc9e79a309769d815ce11172c941d449a2
Abdurauf Ashurov <aabdurauf004@gmail.com>
2023-07-14 01:59:02 +0500

    feat: change styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { Button, ButtonGroup } from "react-bootstrap";
+import { useState, FC } from "react";
 import { ToastContainer, toast } from "react-toastify";
 
 interface TodoItem {
@@ -8,9 +7,11 @@ interface TodoItem {
   completed: boolean;
 }
 
-const App: React.FC = () => {
+const App: FC = () => {
   const [todos, setTodos] = useState<TodoItem[]>([]);
   const [inputVal, setInputVal] = useState<string>("");
+  const [isChecked, setIsChecked] = useState<boolean>(false);
+  const [isEditing, setIsEditing] = useState<boolean>(false);
 
   const addItem = (): void => {
     if (inputVal.trim() !== "") {
@@ -30,41 +31,72 @@ const App: React.FC = () => {
   const deleteItem = (id: string): void => {
     const updateTodos: TodoItem[] = todos.filter(todo => todo.id !== id);
     setTodos(updateTodos);
-    toast.success("Task deleted successfully");
+    toast.error("Task deleted successfully");
   };
 
-  const editItem = (id: string): void => {
-    console.log(id);
+  const handleEditing = (): void => {
+    todos.map(todo => {
+      const oldTodo = {
+        id: todo.id,
+        text: todo.text,
+        completed: todo.completed,
+      };
+      setInputVal(oldTodo.text);
+    });
+    setIsEditing(true);
+  };
+
+  const editItem = (): void => {
+    let newName = inputVal;
+    todos.map(todo => {
+      const newTodo = {
+        id: todo.id,
+        text: newName,
+        completed: todo.completed,
+      };
+      setTodos([...todos, newTodo]);
+    });
+    setInputVal("");
+    setIsEditing(false);
   };
 
   return (
     <div className="background">
       <ToastContainer position="top-left" autoClose={2000} />
-      <div className="container mx-auto">
-        <div className="row col-6 mx-auto mt-5">
+      <div className="container">
+        <h1>To do list</h1>
+        <div className="add-item-wrapper">
           <input onChange={e => setInputVal(e.target.value)} value={inputVal} />
-          <Button className="btn-primary" onClick={addItem}>
-            Add task
-          </Button>
+          <button
+            className={`${isEditing ? "btn-success" : "btn-primary"}`}
+            onClick={isEditing ? editItem : addItem}
+          >
+            {isEditing ? "Save" : "Add task"}
+          </button>
         </div>
-        <ul className="list-group my-5">
+        <ul className="list-group">
           {todos.map(todo => (
             <li
               className="list-group-item d-flex align-items-center justify-content-between"
               key={todo.id}
             >
-              <span>
-                <input type="checkbox" />
-                {todo.text}
-              </span>
-              <ButtonGroup>
-                <Button className="btn-success" onClick={() => editItem(todo.id)}>
+              <div>
+                <input
+                  className="checkbox"
+                  onChange={() => setIsChecked(!isChecked)}
+                  id={todo.id}
+                  type="checkbox"
+                />
+                <label htmlFor={todo.id}>{todo.text}</label>
+              </div>
+              <div>
+                <button className="btn btn-success" onClick={() => handleEditing()}>
                   Edit task
-                </Button>
-                <Button className="btn-danger" onClick={() => deleteItem(todo.id)}>
+                </button>
+                <button className="btn btn-danger" onClick={() => deleteItem(todo.id)}>
                   Delete task
-                </Button>
-              </ButtonGroup>
+                </button>
+              </div>
             </li>
           ))}
         </ul>
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.less";
-import "bootstrap/dist/css/bootstrap.css";
 import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
