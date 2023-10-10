1f05e81d4e6a777ef07e9e1a1481b6904060f5cc
Abdurauf <aabdurauf004@gmail.com>
2023-10-11 00:46:02 +0500

    feat: add redux store

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.less";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
 import "react-toastify/dist/ReactToastify.css";
+import "./index.less";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
